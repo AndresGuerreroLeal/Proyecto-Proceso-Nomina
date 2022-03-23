97a1e11019e34cd5f65fc0bc13f3ffc35906ee64
Juan-CamiloF <jucamilogac@outlook.com>
2022-03-23 15:08:08 -0500

    Se prepara entorno para testing

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,21 +1,24 @@
 /**
  * Archivo de configuración del servidor.
- * 
+ *
  * @author Juan-CamiloF
  */
 
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const log = require("./config/logger")
+const log = require("./config/logger");
 const app = express();
 const { connection } = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use("/api/1.0",require("./routes"))
+app.use("/api/1.0", require("./routes"));
 connection();
-app.listen(PORT,()=>{
-    log.info(`-----Servidor funcionando en puerto ${PORT} ------`);
-})
+
+const server = app.listen(PORT, () => {
+  log.info(`-----Servidor funcionando en puerto ${PORT} ------`);
+});
+
+module.exports = { app, server };
