b230a3b61ed9545c12f0f60340e7b5930b730220
Juan-CamiloF <jucamilogac@outlook.com>
2022-03-29 14:49:38 -0500

    Se agregan validaciones para la información recibida

---
+++ backend/server/validators/auth.js
@@ -0,0 +1,52 @@
+/**
+ * Archivo de validación de la información que llega al servidor
+ *
+ * @author Juan-CamiloF
+ */
+
+const { check } = require("express-validator");
+const validate = require("../helpers/validate");
+
+//Validación de datos de autenticación
+exports.validacionAuth = [
+  check("usuario").exists().notEmpty(),
+  check("contrasenia").exists().notEmpty().isLength({ min: 5 }),
+  (req, res, next) => {
+    validate(req, res, next);
+  },
+];
+
+//Validación de datos de olvide mi contraseña
+exports.validacionForgot = [
+  check("correo").exists().notEmpty().isEmail(),
+  (req, res, next) => {
+    validate(req, res, next);
+  },
+];
+
+//Validación de datos de crear nueva contraseña
+exports.validacionCreate = [
+  check("nuevaContrasenia").exists().notEmpty().isLength({ min: 5 }),
+  (req, res, next) => {
+    validate(req, res, next);
+  },
+];
+
+//Validación de actualizar contraseña
+exports.validacionUpdate = [
+  check("contrasenia").exists().notEmpty().isLength({ min: 5 }),
+  check("nuevaContrasenia").exists().notEmpty().isLength({ min: 5 }),
+  (req, res, next) => {
+    validate(req, res, next);
+  },
+];
+
+//Validación de actualizar información
+exports.validacionUpdateInfo = [
+  check("usuario").exists().notEmpty(),
+  check("nombre").exists().notEmpty().isLength({ min: 3 }),
+  check("correo").exists().notEmpty().isEmail(),
+  (req, res, next) => {
+    validate(req, res, next);
+  },
+];
