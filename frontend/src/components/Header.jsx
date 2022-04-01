e23ef32b6e1f12b8bae41e168324a5965d2c595f
Andres Guerrero <afguerrero@outlook.com>
2022-04-01 18:12:57 -0500

    Añadiento barra de ajustes en perfil

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,10 +13,20 @@ import logo from "../images/logo.png";
 import MailIcon from "@mui/icons-material/Mail";
 import AuthContext from "../context/Auth/AuthContext";
 import FormatoFecha from "../helpers/FormatoFecha";
+import { Button, ListItemIcon, Menu, MenuItem, Tooltip } from "@mui/material";
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+  },
+  acceso:{
+    [theme.breakpoints.down("xs")]: {
+      display:"none"
+    }
   },
   appBar: {
     background: "#02524c",
@@ -27,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     width: "25%",
-    [theme.breakpoints.down("md")]: {
+    [theme.breakpoints.down("sm")]: {
       display: "none",
     },
   },
@@ -49,11 +59,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Header = ({ toggleDrawer }) => {
+  const [ajustes, setAjustes] = useState(false);
+  
+  const open = Boolean(ajustes);
+
+  const handleClick = (event) => {
+    setAjustes(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAjustes(false);
+  };
+
   const classes = useStyles();
 
-  const {auth} = useContext(AuthContext)
-
-  console.log(auth)
+  const { auth } = useContext(AuthContext);
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
@@ -69,8 +88,76 @@ const Header = ({ toggleDrawer }) => {
           <MenuIcon />
         </IconButton>
         <div className={classes.icons}>
-          <p>Ultimo acceso: {FormatoFecha(auth?.ultimoAcceso)}</p>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          
+          <p 
+            className={classes.acceso}
+          >Ultimo acceso: {FormatoFecha(auth?.ultimoAcceso)}</p>
+          <Tooltip title="Ajustes">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 40, height: 40 }}>M</Avatar>
+          </IconButton>
+        </Tooltip>
+        <Menu
+        anchorEl={ajustes}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Actualizar Datos
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Cambiar contraseña
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
         </div>
       </Toolbar>
     </AppBar>
