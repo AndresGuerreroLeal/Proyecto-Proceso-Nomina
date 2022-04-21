4e204711db178a7d70fc679e55a2b3da63114853
Juan-CamiloF <jucamilogac@outlook.com>
2022-04-21 11:35:42 -0500

    Se estandariza archivo para enviar correos

---
+++ backend/server/helpers/enviarCorreos.js
@@ -0,0 +1,71 @@
+/**
+ * Archivo con la configuración para enviar correos
+ *
+ * @author Juan-CamiloF
+ */
+
+const nodemailer = require("nodemailer");
+
+const transport = () => {
+  return nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.CORREO, // generated ethereal user
+      pass: process.env.CONTRASENIA, // generated ethereal password
+    },
+  });
+};
+
+const emailOlvideContrasenia = async (email, nombre, token) => {
+  if (process.env.NODE_ENV !== "test") {
+    let transporter = transport();
+    await transporter.sendMail({
+      from: `Olvido su contraseña <${process.env.CORREO}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: "Olvido su contraseña", // Subject line
+      html: `
+            <h1>Proyecto Proceso Nómina</h1>
+            <br>
+            <p> Hola ${nombre}, se realizó la solicitud de cambio de contraseña, para seguir con el proceso, por favor haga click en el siguiente enlace: 
+              <a href="${process.env.FRONTEND_URL}olvide-contrasenia/${token}" target="_blank">Link</a> 
+            </p>
+           
+            <br>
+            <small>
+              Fundación Universitaria Los Libertadores 
+              <br>
+              Ingeniería de Sistemas
+              <br>
+            </small>
+            `,
+    });
+  }
+};
+
+const emailRegistroEmpleado = async (email, nombre) => {
+  if (process.env.NODE_ENV !== "test") {
+    let transporter = transport();
+    await transporter.sendMail({
+      from: `¡Bienvenido! <${process.env.CORREO}>`,
+      to: `${email}`,
+      subject: "Bienvenido empleado",
+      html: `
+            <h1>Proyecto Proceso Nómina</h1>
+            <br>
+            <p> Hola ${nombre}, ha sido registrado por un administrador dentro del sistema de Proyecto Proceso Nómina.</p>
+
+            <br>
+            <small>
+              Fundación Universitaria Los Libertadores 
+              <br>
+              Ingeniería de Sistemas
+              <br>
+            </small>
+            `,
+    });
+  }
+};
+
+module.exports = { emailOlvideContrasenia, emailRegistroEmpleado };
