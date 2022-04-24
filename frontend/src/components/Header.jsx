e6d8f342ad9ea32cff34913512b1d0fe22d2e17c
Andres Guerrero <afguerrero@outlook.com>
2022-04-24 10:19:30 -0500

    Se realizan cambios de estilos para nuevas funcionalidades

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,10 +11,13 @@ import {
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import Avatar from "@mui/material/Avatar";
+import { Divider, ListItemIcon, Menu, MenuItem, Tooltip } from "@mui/material";
 
 //Material ui icos
 import MenuIcon from "@material-ui/icons/Menu";
-import Avatar from "@mui/material/Avatar";
+import PasswordIcon from '@mui/icons-material/Password';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 //Imagenes
 import logo from "../images/logo.png";
@@ -27,11 +30,11 @@ const useStyles = makeStyles((theme) => ({
     background: "#02524c",
     padding: "15px",
     [theme.breakpoints.up("md")]: {
-      width: "calc(100% - 281px)",
+      width: "calc(100% - 333px)",
     },
   },
   logo: {
-    width: "25%",
+    width: "33%",
     [theme.breakpoints.down("md")]: {
       display: "none",
     },
@@ -58,6 +61,15 @@ const Header = ({ toggleDrawer }) => {
 
   const { perfil } = useContext(AuthContext);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar className={classes.navbar}>
@@ -73,10 +85,74 @@ const Header = ({ toggleDrawer }) => {
         </IconButton>
         <div className={classes.icons}>
           <Typography component="p" variant="p">
-            Ultimo Acceso: {perfil.ultimoAcceso.split("T")[0]}
+            Último Acceso: {perfil.ultimoAcceso.split("T")[0]}
           </Typography>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+          <Avatar alt="Remy Sharp" sx={{ width: 32, height: 32 }}>A</Avatar>
+          </IconButton>
+        </Tooltip>
         </div>
+        <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Mi cuenta
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PasswordIcon fontSize="small"/>
+          </ListItemIcon>
+          Cambiar contraseña
+        </MenuItem>
+            
+        <MenuItem>
+          <ListItemIcon>
+            <LogoutIcon fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
       </Toolbar>
     </AppBar>
   );
