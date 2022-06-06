af8b0d727b3ed7dbcc84832c12a4a121c403f8d0
Juan-CamiloF <jucamilogac@outlook.com>
2022-06-06 14:53:58 -0500

    Corrección de ortografia para comentario de descarga

--- backend/server/routes/report-employee.js
+++ backend/server/routes/report-employee.js
@@ -7,6 +7,7 @@
 const express = require("express");
 const router = express.Router();
 const { auth, admin, reports } = require("../middleware/auth");
+const { validacionId } = require("../validators/reportes");
 /**
  * @see ReportesEmpleadosController
  *
@@ -18,12 +19,11 @@ const {
   listarReportes,
   eliminarReporte,
 } = require("../controllers/reportesEmpleadosController");
-const { validacionId } = require("../validators/reportEmployee");
 
 //Ruta de crear reporte empleado [GET]
 router.get("/create", [auth, reports], crearReporte);
 
-//Ruta de descarga reportes empleados [GET]
+//Ruta de descargar reportes empleados [GET]
 router.get("/download/:file", [auth, reports], descargarReporte);
 
 //Ruta de listar reportes de empleados [GET]
