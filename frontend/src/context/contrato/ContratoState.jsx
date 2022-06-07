dd81bd1a0569da378d308013bfcfc8c4afc3e951
Andres Guerrero <afguerrero@outlook.com>
2022-06-07 16:05:39 -0500

    Se arreglan rutas de navbar para navegación

--- frontend/src/context/contrato/ContratoState.jsx
+++ frontend/src/context/contrato/ContratoState.jsx
@@ -6,10 +6,44 @@ import ContratoContext from "./ContratoContext";
 const ContratoState = ({ children }) => {
   const [contratos,setContratos] = useState([])
 
+  const crearContrato = async (contrato) => {
+    setCargando(true);
+
+    try {
+      const token = localStorage.getItem("token");
+
+      const { data } = await clienteAxios.post(
+        "/api/1.0/contract/create",
+        contrato,
+        TokenAuth(token)
+      );
+
+      setContratos([...contratos, data]);
+
+      mostrarAlerta({
+        message: "El contrato se creo correctamente",
+        categoria: "success",
+      });
+
+      setTimeout(() => {
+        navigate("/home/empleados");
+      }, 2000);
+    } catch (err) {
+      mostrarAlerta({
+        message: err.response.data.message,
+        categoria: "error",
+      });
+      console.log(err.response);
+    } finally {
+      setCargando(false);
+    }
+  };
+
   return (
     <ContratoContext.Provider
       value={{
         contratos,
+        crearContrato
       }}
     >
       {children}
