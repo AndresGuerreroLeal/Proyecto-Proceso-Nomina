d175ebd56d2ff2882ab4bd35f69722ec8d0e5ca3
Andres Guerrero <afguerrero@outlook.com>
2022-06-10 13:11:12 -0500

    Se permite crear la creación de nomina

--- frontend/src/helpers/listaMeses.js
+++ frontend/src/helpers/listaMeses.js
@@ -2,11 +2,7 @@ function listaMeses() {
   const meses = [];
 
   for (let i = 1; i <= 12; i++) {
-    if (i <= 9) {
-      meses.push(`0${i}`);
-    } else {
-      meses.push(i);
-    }
+    meses.push(i);
   }
   return meses;
 }
